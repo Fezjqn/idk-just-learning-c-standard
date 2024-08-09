08e99ffb090f2a5b6d7adceb241bafb03ed43479
fezsu <ferdius.245@gmail.com>
2024-08-09 15:14:52 -0500

    Use Define Statements and a costant number "TIMES", that make my code more flexible.

--- example.c
+++ example.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define TIMES		10
 
 /*
 * Tell the compiler that we intend
@@ -16,7 +17,7 @@ int main(){
 	int count;
 	
 	count = 0;
-	while(count < 10){
+	while(count < TIMES){
 		show_message();
 		count = count + 1;
 	}
